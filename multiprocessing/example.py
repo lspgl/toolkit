4df60f1a9600f3bd900aef0a0f342f9780c57685
lspgl <leo.spiegel@bluewin.ch>
2017-10-31 11:26:58 +0100

    Print speed up

--- multiprocessing/example.py
+++ multiprocessing/example.py
@@ -17,17 +17,22 @@ def main():
     print('Computing parallel')
     # parallelize 'function' over array 'a' with fixed parameters 'exponent' and 'statement'
     b = Parmap(function, a, exponent, print_string=statement)
+    t1 = time.time() - t0
     print('computed b = ', end='')
     print(b)
-    print('Time taken: ', str(round(time.time() - t0, 2)))
+    print('Time taken: ', str(round(t1, 2)))
     print()
 
-    t0 = time.time()
+    t2 = time.time()
     print('Computing serial')
     b_s = [function(a_s, exponent, print_string=statement) for a_s in a]
+    t3 = time.time() - t2
     print('computed b = ', end='')
     print(b_s)
-    print('Time taken: ', str(round(time.time() - t0, 2)))
+    print('Time taken: ', str(round(t3, 2)))
+    print()
+    speedup = t3 / t1
+    print('Speedup x' + str(round(speedup, 2)))
 
 
 if __name__ == '__main__':

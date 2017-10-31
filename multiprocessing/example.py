0788ad329c48a316bac3286cef1e6801bff7535c
lspgl <leo.spiegel@bluewin.ch>
2017-10-31 11:18:49 +0100

    MP comparison example

--- multiprocessing/example.py
+++ multiprocessing/example.py
@@ -4,7 +4,7 @@ import time
 
 def function(x, fixed, print_string='None'):
     print(print_string)
-    print('simulating long calculation...')
+    # simulating long calculation
     time.sleep(1)
     return x**fixed
 
@@ -12,10 +12,21 @@ def function(x, fixed, print_string='None'):
 def main():
     a = [0, 1, 10, 15]
     exponent = 2
+    statement = 'Im a function call!'
     t0 = time.time()
-    b = Parmap(function, a, exponent, print_string='Im a process')
+    print('Computing parallel')
+    # parallelize 'function' over array 'a' with fixed parameters 'exponent' and 'statement'
+    b = Parmap(function, a, exponent, print_string=statement)
     print('computed b = ', end='')
     print(b)
+    print('Time taken: ', str(round(time.time() - t0, 2)))
+    print()
+
+    t0 = time.time()
+    print('Computing serial')
+    b_s = [function(a_s, exponent, print_string=statement) for a_s in a]
+    print('computed b = ', end='')
+    print(b_s)
     print('Time taken: ', str(round(time.time() - t0, 2)))
 
 
